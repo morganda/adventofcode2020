3e0e86abe6b12319cd58265d127fcb9ba41c317e
Morgan Adams <morgan.d.adams@gmail.com>
2020-12-06 18:21:02 -0800

    some cleanup on 5 and 6

--- day5.py
+++ day5.py
@@ -35,11 +35,7 @@ def get_seat_id(code):
     return sid
 
 
-if __name__ == '__main__':
-    with open(filename, 'r') as fin:
-        lines = fin.read().split('\n')
-
-    # seat map for part 2
+def init_seat_map():
     seat_map = dict()
     i = 0
     j = 0
@@ -49,19 +45,37 @@ if __name__ == '__main__':
             j += 1
         j = 0
         i += 1
+    return seat_map
 
+
+def get_largest_sid(codes):
     largest_sid = 0
-    for line in lines:
-        seat = get_seat_id(line)
+    seat_map = init_seat_map()
+    for code in codes:
+        seat = get_seat_id(code)
         del seat_map[seat]
-        largest_sid = max(largest_sid, get_seat_id(line))  # part 1
+        largest_sid = max(largest_sid, get_seat_id(code))
+    return largest_sid, seat_map
 
+
+def get_my_seat_id(seat_map):
     my_sid = -1
     for sid in seat_map.keys():
         if sid != my_sid + 1:
             my_sid = sid
             break
         my_sid = sid
+    return my_sid
 
+
+if __name__ == '__main__':
+    with open(filename, 'r') as fin:
+        lines = fin.read().split('\n')
+
+    # part 1
+    largest_sid, seat_map = get_largest_sid(lines)
     print(f'Largest seat ID: {largest_sid}')
+
+    # part 2
+    my_sid = get_my_seat_id(seat_map)
     print(f'My seat ID: {my_sid}')
