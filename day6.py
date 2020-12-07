976080fcc908f49b22ac02cd70bd37b798456a9a
Morgan Adams <morgan.d.adams@gmail.com>
2020-12-06 18:12:42 -0800

    clean up part 6 a bit

--- day6.py
+++ day6.py
@@ -4,34 +4,34 @@
 filename = 'inputs/input6.txt'
 
 
-if __name__ == '__main__':
-    with open(filename, 'r') as fin:
-        lines = fin.read().split('\n')
-
+def count_yeses(submissions):
     sum = 0
 
     # part 1
     yes_lookup = set()
-    for line in lines:
-        if len(line.strip()):
-            for i in line:
+    for submission in submissions:
+        if len(submission.strip()):
+            for i in submission:
                 yes_lookup.add(i)
         else:
             sum += len(yes_lookup)
             yes_lookup = set()
+
+    # add last grouping
     sum += len(yes_lookup)
 
-    print(f'Sum of Counts: {sum}')
+    return sum
 
-    # part 2
+
+def count_yeses_part2(submissions):
     sum = 0
     people_count = 0
     yes_lookup = dict()
 
-    for line in lines:
-        if len(line.strip()):
+    for submission in submissions:
+        if len(submission.strip()):
             people_count += 1
-            for i in line:
+            for i in submission:
                 if i not in yes_lookup:
                     yes_lookup[i] = 1
                 else:
@@ -43,8 +43,21 @@ if __name__ == '__main__':
             people_count = 0
             yes_lookup = dict()
 
+    # add last grouping
     for _, count in yes_lookup.items():
         if count == people_count:
             sum += 1
 
+    return sum
+
+
+if __name__ == '__main__':
+    with open(filename, 'r') as fin:
+        lines = fin.read().split('\n')
+
+    sum = count_yeses(lines)
+    print(f'Sum of Counts: {sum}')
+
+    # part 2
+    sum = count_yeses_part2(lines)
     print(f'Sum of Counts Part 2: {sum}')
